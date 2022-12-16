70c238a6d9bc3911d04c4243323baca89c25168e
Serenajandra <serena.jandra@gmail.com>
2022-12-16 17:57:57 +0100

    aggiunto style con scss

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,17 +2,16 @@
 
 @section('content')
     <section>
-        <h1 class="text-center">Movies</h1>
+        <h1 class="text-center text-light m-4 text-uppercase">Movies</h1>
         <div class="container">
-            <div class="row row-cols-4 justify-content-between">
+            <div class="row justify-content-between align-items-start">
                 @foreach ($movies as $movie)
-                    <div class="card" style="width: 18rem;">
+                    <div class="card bg-dark " style="width: 16rem;">
                         <img src="{{ $movie->image }}" class="card-img-top" alt="...">
                         <div class="card-body">
                             <h5 class="card-title">{{ $movie->title }}</h5>
-                            {{-- <p class="card-text">{{$movie->}}}}</p> --}}
                         </div>
-                        <ul class="list-group list-group-flush">
+                        <ul class="list-group-flush list-group ">
                             <li class="list-group-item">
                                 <h6>Titolo originale</h6> {{ $movie->original_title }}
                             </li>
@@ -26,12 +25,11 @@
                                 <h6>Voto</h6>{{ $movie->vote }}
                             </li>
                         </ul>
-                        <div class="card-body">
+                        {{-- <div class="card-body">
                             <a href="#" class="card-link">Card link</a>
                             <a href="#" class="card-link">Another link</a>
-                        </div>
+                        </div> --}}
                     </div>
-                    {{-- <div class="col"></div> --}}
                 @endforeach
             </div>
 
